2c0ee8e3ab35ad0e0905bb19c93e2b47f4f2051c
Thanos-in <sayyodbee2006@gmail.com>
2025-06-05 14:02:42 +0500

    Add .gitignore file

---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/tailwind.css'
+
+createApp(App).mount('#app')
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Salom, Vue Router va Pinia o‘rnatildi!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
