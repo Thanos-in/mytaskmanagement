8ed2b43157d7708ed1003154bab8f646594f4f8d
Thanos-in <sayyodbee2006@gmail.com>
2025-06-06 12:06:20 +0500

    Add .gitignore file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,10 @@
 import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
 import './assets/tailwind.css'
-
-createApp(App).mount('#app')
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import router from 'router'
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
